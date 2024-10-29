975388d0042550fce63ae537b7c07e2a41c49707
adityajais8090 <a_jaiswal@ece.iitr.ac.in>
2024-10-29 20:52:34 +0530

    Add env file

--- src/api/data.jsx
+++ src/api/data.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_BACKEND;
 
 
 export const allData = async () => {
     try {
-        const response = await axios.get("https://api.quicksell.co/v1/internal/frontend-assignment");
+        const response = await axios.get(`${API_URL}`);
         return response.data; // Access data directly
     } catch (err) {
         console.error("Error in Retrieving Data:", err.response ? err.response.data : err);
